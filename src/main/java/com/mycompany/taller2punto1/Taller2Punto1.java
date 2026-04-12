43d55b40177ac48a67108b64f1ddacb8b0ac93af
Rosa Maria Peña <rosamariapca10@gmail.com>
2026-04-12 18:40:45 -0500

    Creacion del proyecto

---
+++ src/main/java/com/mycompany/taller2punto1/Taller2Punto1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.taller2punto1;
+
+/**
+ *
+ * @author rosam
+ */
+public class Taller2Punto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
